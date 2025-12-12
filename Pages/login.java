cbadac047612e8d925d0d62a8cb8e1c7c7ae4556
niuerben <niuerben@outlook.com>
2025-12-12 08:46:31 +0800

    更新swing组件的调试

--- Pages/login.java
+++ Pages/login.java
@@ -1,6 +1,17 @@
 import javax.swing.*;
 public class login{
     public static void main(String[] args){
-        System.out.println("hello world!\n");
+        // 创建及设置窗口
+        JFrame frame = new JFrame("欢迎使用AscentSys应用");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        System.out.println("日志信息：系统\n");
+
+        // 添加  标签
+        JLabel label = new JLabel("请继续开发一下内容");
+        frame.getContentPane().add(label);
+
+        frame.pack();
+        frame.setVisible(true);
     }
 }
